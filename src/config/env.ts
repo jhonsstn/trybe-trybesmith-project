ad5f53f8d744002dd621febce06d20e1a29086b7
Jhon Santana <jhonsstn@gmail.com>
2022-07-29 23:32:13 -0300

    fix: add default value to jwt secret

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,5 +6,5 @@ export default {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET || 'secret',
 };
